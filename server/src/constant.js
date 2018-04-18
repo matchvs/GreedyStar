d0ad341d9eefb130d228c65bb42b7d951c5fb556
semir <1344534343@qq.com>
2018-04-18 10:35:36 +0800

    add server

---
+++ server/src/constant.js
@@ -0,0 +1,13 @@
+module.exports = {
+    READY_TO_GAME_START_EVENT: 'ready_to_game_start_event',
+    CLOSE_READY_TO_GAME_START_EVENT: 'close_ready_to_game_start_event',
+
+    READY_GAME_TIME_EVENT: 'ready_game_time_event',
+    // GAME_TIME_EVENT: 'game_time_event',
+    CANNOT_LEAVE_ROOM_EVENT: 'cannot_leave_room_event',
+
+    GAME_START_EVENT: 'game_start_event',
+    GAME_HAS_START_EVENT: 'game_has_start_event',
+
+    SYSTEM_ROOM_MIN_PLAYER_COUNT: 3,
+};
